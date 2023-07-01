ac40255e09f92f4d9e7e1a5f94ee4dddf189ee9d
Karina <Emilypoison@mail.ru>
2023-07-01 16:40:42 +0300

    outline and bit bar

--- NoNameProject/Assets/Scripts/MusicBit.cs
+++ NoNameProject/Assets/Scripts/MusicBit.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class MusicBit : MonoBehaviour
 {
     [SerializeField] private BackgroundMusicSO music;
+    [SerializeField] private Transform bitBar;
     private AudioSource _audioSource;
     
     public delegate void AccountHandler(float message);
@@ -14,9 +15,14 @@ public class MusicBit : MonoBehaviour
     //public event Action ActionBitHit;
     private float nextActionTime;
     private float period;
+
+    private Color currentBitBarColor;
+    private Color redColor = Color.red;
+    private float previousBit;
     
     void Start()
     {
+        currentBitBarColor = bitBar.GetComponent<SpriteRenderer>().color;
         _audioSource = GetComponent<AudioSource>();
         nextActionTime = 0;
         period = (60 / music.BMP) * music.bit;
@@ -27,13 +33,31 @@ public class MusicBit : MonoBehaviour
     
     void Update()
     {
+        
         if (Time.time >= nextActionTime )
         {
-            var pre = nextActionTime;
+            previousBit = nextActionTime;
             nextActionTime += period;
-            Debug.Log("Bit");
-            Debug.Log(nextActionTime - pre);
-            ActionBitHit?.Invoke(pre);
+            bitBar.GetComponent<SpriteRenderer>().color = Color.Lerp(currentBitBarColor, redColor, 0.5f);
+            //ChangeBitBarColor(redColor);
+            
+            //Debug.Log("Bit");
+            //Debug.Log(nextActionTime - pre);
+            ActionBitHit?.Invoke(previousBit);
         }
+        
+        //Debug.Log(Time.time + " Time time");
+        //Debug.Log(pre + 0.03f + " next Time");
+        else if (Time.time >= previousBit + 0.3f)
+        {
+            ChangeBitBarColor(currentBitBarColor);
+            //bitBar.GetComponent<SpriteRenderer>().color = Color.Lerp(redColor,currentBitBarColor, Mathf.Abs(Mathf.Sin(Time.time) * 2));
+        }
+
+    }
+
+    private void ChangeBitBarColor(Color color)
+    {
+        bitBar.GetComponent<SpriteRenderer>().color = color;
     }
 }
