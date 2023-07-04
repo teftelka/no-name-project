6d8153c38cec658b938711e7651d340eb2ae892f
Karina <Emilypoison@mail.ru>
2023-07-04 17:00:52 +0300

    chahge enemy collider

--- NoNameProject/Assets/Scripts/Enemy.cs
+++ NoNameProject/Assets/Scripts/Enemy.cs
@@ -7,17 +7,15 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private int health;
-    private int maxHealth = 300000;
+    private int maxHealth = 300;
 
     private Animator animator;
     public bool IsDead;
-    private AdvancedPolygonCollider _collider;
-    
+
     void Start()
     {
         health = maxHealth;
         animator = GetComponent<Animator>();
-        _collider = GetComponent<AdvancedPolygonCollider>();
     }
 
     public void TakeDamage(int damageAmount, bool isCriticalDamage)
@@ -25,25 +23,18 @@ public class Enemy : MonoBehaviour
         if (!IsDead)
         {
             health -= damageAmount;
-            _collider.RunInPlayMode = false;
             animator.SetTrigger("Hurt");
             DamagePopup.Create(transform.position, damageAmount, isCriticalDamage);
 
             if (health <= 0)
             {
                 IsDead = true;
-                _collider.RunInPlayMode = false;
                 animator.SetBool("IsDead", true);
                 gameObject.GetComponent<EnemyPatrol>().StopPatrolling();
             }
         }
     }
 
-    private void EnableRunInPlayModeCollider()
-    {
-        //_collider.RunInPlayMode = true;
-    }
-    
     private void Death()
     {
         Destroy(gameObject);
