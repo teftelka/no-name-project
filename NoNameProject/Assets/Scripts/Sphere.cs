c888eb1893228dc94bc838330ad440fcb779cf47
Karina <44575378+teftelka@users.noreply.github.com>
2023-08-02 18:55:35 +0300

    sphere deleted correctly

--- NoNameProject/Assets/Scripts/Sphere.cs
+++ NoNameProject/Assets/Scripts/Sphere.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 using CodeMonkey.Utils;
 
@@ -9,9 +10,16 @@ public class Sphere : MonoBehaviour
     private Vector3 shootDir;
     [SerializeField] private int sphereDamage = 100;
     List<Enemy> enemies = new List<Enemy>();
+
+    [SerializeField] private Camera camera;
     
     private Color criticalHitSphereColor = UtilsClass.GetColorFromString("0EF4FF");
-    
+
+    private void Start()
+    {
+        camera = FindObjectOfType<Camera>();
+    }
+
     public void Setup(Vector3 shootDir, bool isCriticalHit)
     {
         this.shootDir = shootDir;
@@ -20,14 +28,18 @@ public class Sphere : MonoBehaviour
             sphereDamage *= 2;
             gameObject.GetComponentInChildren<SpriteRenderer>().color = criticalHitSphereColor;
         }
-        
-        Destroy(gameObject, 1f);
     }
 
     private void Update()
     {
         float moveSpeed = 30f;
         transform.position += shootDir * moveSpeed * Time.deltaTime;
+        
+        Vector3 point = camera.WorldToViewportPoint(transform.position);
+        if (point.x < 0f || point.x > 1f)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D col)
